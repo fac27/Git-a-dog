de480b3aa743e043bdcba8a9daf0feef4b4c7df0
eliazzo <bethazz@hotmail.co.uk>
2023-02-10 20:57:24 +0000

    First attempt accessing dog API

--- match.js
+++ match.js
@@ -9,3 +9,22 @@ function getUser(name){
   getUser("eliazzo")
   .then((user) => console.log(user))
   .catch((error) => console.log(error))
+
+
+  // Test access to dog API
+
+  function getDog(breed){
+    fetch(`https://dog.ceo/api/${breed}/image/random`)
+    return fetch(`https://dog.ceo/api/${breed}/image/random`)
+    .then((response) => response.json())
+  }
+
+  getDog("poodle-standard")
+    .then((dog) => console.log(dog))
+    .catch((error) => console.log(error))
+
+    // This produces an error "blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource."
+
+  
+
+ 
