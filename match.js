3a3f4637c030cf0863ceb451e7950870431e3bf6
cameo choquer <cjchoquer@gmail.com>
2023-02-11 15:55:35 +0000

    added function to interact with elements

--- match.js
+++ match.js
@@ -1,11 +1,28 @@
 // Get github user using fetch
 
-function getUser(name){
-    fetch(`https://api.github.com/users/${name}`)
-    return fetch(`https://api.github.com/users/${name}`)
-    .then ((response) => response.json()) // access the response using json() method
-  }
+let userName = document.getElementById('user-name');
 
-  getUser("eliazzo")
-  .then((user) => console.log(user))
-  .catch((error) => console.log(error))
+userName.addEventListener('keydown', (e) =>{
+  if(e.key === "Enter"){
+    e.preventDefault();
+    let inputValue = userName.value;
+    fetch(`https://api.github.com/users/${inputValue}`)
+    return fetch(`https://api.github.com/users/${inputValue}`)
+    .then ((response) => response.json()) // access the response using json() method
+    .then(alert(`Enter Key is Pressed, ${inputValue}`))
+    .then((user) => console.log(user))
+    .catch((error) => console.log(error))
+ }
+ })
+
+
+
+// function getUser(name){
+//     fetch(`https://api.github.com/users/${name}`)
+//     return fetch(`https://api.github.com/users/${name}`)
+//     .then ((response) => response.json()) // access the response using json() method
+//   }
+
+  // getUser(inputValue)
+  // .then((user) => console.log(user))
+  // .catch((error) => console.log(error))
