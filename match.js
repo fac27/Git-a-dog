c9dfe95bd2cd2f796b07467c51a7e07b2fbc5e81
cameo choquer <cjchoquer@gmail.com>
2023-02-11 15:55:35 +0000

    added function to interact with elements

--- match.js
+++ match.js
@@ -1,8 +1,30 @@
 // Get github user using fetch
 
-async function getUser(name){
-    fetch(`https://api.github.com/users/${name}`)
-    return fetch(`https://api.github.com/users/${name}`)
+let userName = document.getElementById('user-name');
+
+userName.addEventListener('keydown', (e) =>{
+  if(e.key === "Enter"){
+    e.preventDefault();
+    let inputValue = userName.value;
+    fetch(`https://api.github.com/users/${inputValue}`)
+    return fetch(`https://api.github.com/users/${inputValue}`)
+    .then((response) => response.json()) // access the response using json() method
+    .then(alert(`Enter Key is Pressed, ${inputValue}`))
+    .then((user) => console.log(user))
+    .then(() => {
+      let y = inputValue.split('', 1); 
+      console.log(y)})
+    .catch((error) => console.log(error))
+ }
+ })
+
+//  let y = inputValue.split(1);
+//  console.log(y)
+
+
+ 
+    fetch(`https://dog.ceo/api/breeds/list/all`)
+    // return fetch(`https://dog.ceo/api/breeds/list/all`)
     .then ((response) => response.json()) // access the response using json() method
   }
 
@@ -39,3 +61,8 @@ async function getUser(name){
     })
     .catch((error) => console.log(error))
 
+
+
+  
+
+ 
