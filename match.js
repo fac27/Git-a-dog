03e9ea7aff97100cf247ea889f94c355c6c8f103
cameo choquer <cjchoquer@gmail.com>
2023-02-12 16:26:05 +0000

    fetching dogs and using split to get first char

--- match.js
+++ match.js
@@ -8,6 +8,7 @@ userName.addEventListener('keydown', (e) =>{
     let inputValue = userName.value;
     fetch(`https://api.github.com/users/${inputValue}`)
     return fetch(`https://api.github.com/users/${inputValue}`)
+<<<<<<< HEAD
     .then((response) => response.json()) // access the response using json() method
     .then(alert(`Enter Key is Pressed, ${inputValue}`))
     .then((user) => console.log(user))
@@ -33,6 +34,9 @@ userName.addEventListener('keydown', (e) =>{
     let inputValue = userName.value;
     fetch(`https://api.github.com/users/${inputValue}`)
     return fetch(`https://api.github.com/users/${inputValue}`)
+=======
+<<<<<<< HEAD
+>>>>>>> 09b34aa (fetching dogs and using split to get first char)
     .then ((response) => response.json()) // access the response using json() method
     .then((response) => 
           {let breeds = Object.keys(response.message)
@@ -54,6 +58,41 @@ userName.addEventListener('keydown', (e) =>{
   }
 
   getUser("eliazzo")
+=======
+    .then((response) => response.json()) // access the response using json() method
+    .then(alert(`Enter Key is Pressed, ${inputValue}`))
+    .then((user) => console.log(user))
+    .then(() => {
+      let y = inputValue.split('', 1); 
+      console.log(y)})
+    .catch((error) => console.log(error))
+ }
+ })
+
+//  let y = inputValue.split(1);
+//  console.log(y)
+
+
+ 
+    fetch(`https://dog.ceo/api/breeds/list/all`)
+    // return fetch(`https://dog.ceo/api/breeds/list/all`)
+    .then ((response) => response.json()) // access the response using json() method
+    .then((response) => 
+          {let x = response.message
+          console.log(x.buhund)});
+  
+  // getUser(inputValue)
+  // .then((user) => console.log(user))
+  // .catch((error) => console.log(error))
+
+// function getUser(name){
+//     fetch(`https://api.github.com/users/${name}`)
+//     return fetch(`https://api.github.com/users/${name}`)
+//     .then ((response) => response.json()) // access the response using json() method
+//   }
+
+  // getUser(inputValue)
+>>>>>>> 1c59183 (fetching dogs and using split to get first char)
   // .then((user) => console.log(user))
   .then((userData) => {
     const heading = document.createElement("h2")
