1c5918354a6da2632417c98a60a8a1cb5f02eb79
cameo choquer <cjchoquer@gmail.com>
2023-02-12 16:26:05 +0000

    fetching dogs and using split to get first char

--- match.js
+++ match.js
@@ -8,14 +8,31 @@ userName.addEventListener('keydown', (e) =>{
     let inputValue = userName.value;
     fetch(`https://api.github.com/users/${inputValue}`)
     return fetch(`https://api.github.com/users/${inputValue}`)
-    .then ((response) => response.json()) // access the response using json() method
+    .then((response) => response.json()) // access the response using json() method
     .then(alert(`Enter Key is Pressed, ${inputValue}`))
     .then((user) => console.log(user))
+    .then(() => {
+      let y = inputValue.split('', 1); 
+      console.log(y)})
     .catch((error) => console.log(error))
  }
  })
 
+//  let y = inputValue.split(1);
+//  console.log(y)
 
+
+ 
+    fetch(`https://dog.ceo/api/breeds/list/all`)
+    // return fetch(`https://dog.ceo/api/breeds/list/all`)
+    .then ((response) => response.json()) // access the response using json() method
+    .then((response) => 
+          {let x = response.message
+          console.log(x.buhund)});
+  
+  // getUser(inputValue)
+  // .then((user) => console.log(user))
+  // .catch((error) => console.log(error))
 
 // function getUser(name){
 //     fetch(`https://api.github.com/users/${name}`)
